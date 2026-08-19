667a47f1acbd77b1faa25df6dfc0fdc06eda81c1
divinabruxa.github.io <herculesfardim@hotmail.com>
2026-08-19 12:56:54 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,1 @@
-self.addEventListener("install",()=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil((async()=>{
-  try{const ks=await caches.keys();await Promise.all(ks.map(k=>caches.delete(k)))}catch(e){}
-  try{await self.registration.unregister()}catch(e){}
-})()));
+const CACHE='orbe-flat-v1';const FILES=['./','index.html','tarot-atlas.webp','manifest.webmanifest','icone.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./'))))});
