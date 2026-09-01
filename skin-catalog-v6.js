134180ce9fa6e272c59c95a69c472ae02aa909ad
divinabruxa.github.io <herculesfardim@hotmail.com>
2026-09-01 05:17:53 -0300

    Add files via upload

--- skin-catalog-v6.js
+++ skin-catalog-v6.js
@@ -4,3 +4,4 @@ export let SKINS_V6=Object.freeze([
 SKINS_V6=Object.freeze([...SKINS_V6,{id:'solstice',name:'Solstício Dourado',priceCents:2990,index:21,status:'planned',cosmeticOnly:true}]);
 SKINS_V6=Object.freeze([...SKINS_V6,{id:'neptune',name:'Maré de Netuno',priceCents:3490,index:22,status:'planned',cosmeticOnly:true}]);
 SKINS_V6=Object.freeze([...SKINS_V6,{id:'enchanted-forest',name:'Floresta Encantada',priceCents:3990,index:23,status:'planned',cosmeticOnly:true}]);
+SKINS_V6=Object.freeze([...SKINS_V6,{id:'cosmic-dragon',name:'Dragão Cósmico',priceCents:4490,index:24,status:'planned',cosmeticOnly:true}]);
